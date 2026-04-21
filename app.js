7075ab95b5d8dabff991b0ee883cd36ff3a93f56
JOSEA2990 <joseamartinezg.2990@gmail.com>
2026-04-20 23:22:42 -0500

    Update3.2

--- app.js
+++ app.js
@@ -77,11 +77,11 @@ async function registrar(){
     celular:document.getElementById("celular").value
   };
 
-  const res = await fetch("https://script.google.com/macros/s/AKfycbwPF3pLAnltAI7OkmXF5sLqZT7kSyV_l_fLIP23c3skpNpG3pi2jdQcd84J7f8uchA0iQ/exec",{
-    method:"POST",
-    headers:{ "Content-Type":"application/json"},
-    body: JSON.stringify(datos)
-  });
+  await fetch("https://script.google.com/macros/s/AKfycbwPF3pLAnltAI7OkmXF5sLqZT7kSyV_l_fLIP23c3skpNpG3pi2jdQcd84J7f8uchA0iQ/exec", {
+  method: "POST",
+  mode: "no-cors",
+  body: JSON.stringify(datos)
+});
 
   const json = await res.json();
 
