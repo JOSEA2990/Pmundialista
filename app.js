9d0ee6004e55c65156565a8ea1a8b4daad04e869
JOSEA2990 <joseamartinezg.2990@gmail.com>
2026-04-21 20:42:48 -0500

    Update4.6

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ async function registrar() {
   try {
 
     await fetch(
-      "https://script.google.com/macros/s/AKfycbwPF3pLAnlAI7OkmXF5sLqZT7kSyV_l_fLIP23c3skpNpG3pi2jdQcd84J7f8uchA0iQ/exec",
+      "https://script.google.com/macros/s/AKfycbwPF3pLAnltAI7OkmXF5sLqZT7kSyV_l_fLIP23c3skpNpG3pi2jdQcd84J7f8uchA0iQ/exec",
       {
         method: "POST",
         body: datos
