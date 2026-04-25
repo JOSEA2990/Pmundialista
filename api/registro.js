03d4958c95ef4ca6d50a115c82db91f01ec2bf37
JOSEA2990 <joseamartinezg.2990@gmail.com>
2026-04-25 15:26:39 -0500

    Update4.11

--- api/registro.js
+++ api/registro.js
@@ -1,14 +1,17 @@
 export default async function handler(req, res) {
 
-  const response = await fetch(
-    "https://script.google.com/macros/s/AKfycbwPF3pLAnltAI7OkmXF5sLqZT7kSyV_l_fLIP23c3skpNpG3pi2jdQcd84J7f8uchA0iQ/exec",
-    {
-      method: "POST",
-      body: new URLSearchParams(req.body)
-    }
-  );
+  const url =
+    "https://script.google.com/macros/s/AKfycbwPF3pLAnltAI7OkmXF5sLqZT7kSyV_l_fLIP23c3skpNpG3pi2jdQcd84J7f8uchA0iQ/exec";
 
-  const text = await response.text();
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams(req.body).toString(),
+  });
 
-  res.status(200).send(text);
+  const data = await response.text();
+
+  res.status(200).send(data);
 }
