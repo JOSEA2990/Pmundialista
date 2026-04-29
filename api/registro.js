d6d2e24c632dd556a819e206d2b952a1e0a2499c
JOSEA2990 <joseamartinezg.2990@gmail.com>
2026-04-29 10:54:50 -0500

    Update5.1

--- api/registro.js
+++ api/registro.js
@@ -1,23 +1,29 @@
 export default async function handler(req, res) {
 
-  if(req.method !== "POST"){
-    return res.status(405).json({error:"Metodo no permitido"});
+  try {
+
+    const form = await req.formData();
+    const datos = Object.fromEntries(form);
+
+    const respuesta = await fetch(
+      "https://script.google.com/macros/s/AKfycbxyrSg2PkETwI9ZxyBWebngL14g9bPG--nvmJzGYt0eegI0E-fhvmTGy1ihcSBxhtANgA/exec",
+      {
+        method:"POST",
+        body:new URLSearchParams(datos)
+      }
+    );
+
+    const texto = await respuesta.text();
+
+    res.status(200).send(texto);
+
+  } catch(err){
+
+    console.error(err);
+    res.status(500).json({
+      error:"Error en API",
+      detalle: err.toString()
+    });
+
   }
-
-  const form = await req.formData();
-
-  const datos = Object.fromEntries(form);
-
-  // 👉 llamar a Google Apps Script
-  const respuesta = await fetch(
-    "https://script.google.com/macros/s/AKfycbxyrSg2PkETwI9ZxyBWebngL14g9bPG--nvmJzGYt0eegI0E-fhvmTGy1ihcSBxhtANgA/exec",
-    {
-      method:"POST",
-      body:new URLSearchParams(datos)
-    }
-  );
-
-  const texto = await respuesta.text();
-
-  res.status(200).send(texto);
 }
